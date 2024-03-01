a1b4a7e94a828ebe3cbe19ac1d955dca41dc88f3
duyneeee <148990623+duyneeee@users.noreply.github.com>
2024-03-01 16:44:42 +0700

    Add files via upload

--- avacado.py
+++ avacado.py
@@ -23,3 +23,10 @@ title = "Bar Graph",
 color="Large Bags",
 orientation='h'
 )
+#Bar Color
+bar_graph = px.bar(
+x = al_df["Date"],
+y = al_df["Large Bags"],
+title = "Bar Graph",
+color=al_df["Large Bags"]
+)
